ec40d6cc88e4e8c449dbfee212e4ceb826be02a3
Lemon <sabinbasi111@gmail.com>
2024-08-08 10:10:56 +0545

    login register

--- src/main/java/appSoft/project/controller/AdminController.java
+++ src/main/java/appSoft/project/controller/AdminController.java
@@ -2,10 +2,16 @@ package appSoft.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import appSoft.project.model.Admin;
 import appSoft.project.model.SalaryPayment;
+import appSoft.project.service.AdminService;
 import appSoft.project.service.ExpenseService;
 import appSoft.project.service.FeesPaymentService;
 import appSoft.project.service.SalaryPaymentService;
@@ -20,11 +26,41 @@ public class AdminController {
 	SalaryPaymentService salaryPaymentService;
 	@Autowired
 	FeesPaymentService feesPaymentService;
+	@Autowired
+	AdminService adminService;
 	
 	@GetMapping("/")
 	private String getIndex() {
-		return "AdminDashBoard";
+		return "Login";
 	}
+	@PostMapping("/login")
+	private String postLogin(@ModelAttribute Admin admin, Model model) {
+		Admin adminCheck=adminService.findByUsernameAndPassword(admin.getUsername(), admin.getPassword());
+		if(adminCheck==null) {
+			model.addAttribute("LoginMessage","user doesnot exist");
+			return"Login";
+			
+		}
+		else return"AdminDashboard";
+	}
+	
+	@GetMapping("/register")
+	private String register() {
+		return"Register";
+	}
+	
+	@PostMapping("/register")
+	private String postRegister(@ModelAttribute Admin admin,Model model) {
+		Admin adminCheck = adminService.findByUsername(admin.getUsername());
+		if(adminCheck!=null) {
+			model.addAttribute("message","username already exists");
+			return"Register";
+		}
+		adminService.addAdmin(admin);
+		return "redirect:/";
+	}
+	
+	
 	@GetMapping("/financeExcel")
 	public  ModelAndView excel() {
 		ModelAndView mv =new ModelAndView();
